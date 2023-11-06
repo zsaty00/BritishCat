840b7722fd9c161b42300538517acbced89b0624
zsaty00 <satylgan.dev@gmail.com>
2023-11-05 22:23:49 -0600

    Adding new project to github

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello John!");
+        System.out.println("Hello Begim!");
+        System.out.println("Hello Aichurek!");
+        System.out.println("Hello world!");
+    }
+}
+
+
+
+
